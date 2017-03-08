f2d5b5483fa8b7aaa5fd76fc5abb112ca93aeefb
LoranDeMarcus <lorandemarcus@gmail.com>
2017-03-08 18:58:45 +0300

    сделал добавление элементов списка

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 /* Добавление элемента списка */
+var tasks = [ ];
 
 $('#new-todo').keyup(function(e){ console.log(e.keyCode);
     if(e.keyCode==13){
         $('#main').show();
         $('#footer').show();
         $('#todo-list').append('<li>\
-                <div class="view">\
-                    <input class="toggle" type="checkbox">'+$(this).val()+'<label> </label>\
+                <div class="todo-task">\
+                  <label><input class="toggle" type="checkbox">'+$(this).val()+'</label>\
                     <button class="destroy"></button>\
                 </div>\
            </li>');
     }
 });
+
+document.write(tasks.length);
 
 /* Удаление */
 
