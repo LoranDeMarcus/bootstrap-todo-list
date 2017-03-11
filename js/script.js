78954a3c00a6aea07e31b9931e39f376f7e2a677
LoranDeMarcus <lorandemarcus@gmail.com>
2017-03-11 21:07:57 +0300

    необходимо исправить работу массива

--- js/script.js
+++ js/script.js
@@ -1,23 +1,32 @@
 /* Добавление элемента списка */
-var tasks = [ ];
+
+var tasks = [];
 
 $('#new-todo').keyup(function(e){ console.log(e.keyCode);
-    if(e.keyCode==13){
+
+    if (e.keyCode==13){
         $('#main').show();
         $('#footer').show();
         $('#todo-list').append('<li>\
                 <div class="todo-task">\
-                  <label><input class="toggle" type="checkbox">'+$(this).val()+'</label>\
+                  <label class="text"><input class="toggle" type="checkbox">'+$(this).val()+'</label>\
                     <button class="destroy"></button>\
                 </div>\
            </li>');
+        // Добавление в массив
+        $('label.text').each(function(){
+            tasks.push($(this).text());
+        });
+        // Очистка Input
+        $('#new-todo').val('');
     }
 });
-
-document.write(tasks.length);
 
 /* Удаление */
 
 $('#todo-list').on('click','button.destroy',function(){
     $(this).parent().parent().remove();
+    if ((tasks) < 1 ) {
+        $('#footer').hide();
+    }
 });
