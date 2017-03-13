c07f5663bdaeb993e87e419d2d552f9aef7156e8
LoranDeMarcus <lorandemarcus@gmail.com>
2017-03-13 11:42:37 +0300

    добавить сортировку

--- js/script.js
+++ js/script.js
@@ -2,10 +2,9 @@
 
 var tasks = [];
 
-$('#new-todo').keyup(function(e){ console.log(e.keyCode);
+$('#new-todo').keyup(function(event){
 
-    if (e.keyCode==13){
-        var $this = $(this);
+    if (event.keyCode==13){
         $('#main').show();
         $('#footer').show();
         $('#todo-list').append('<li>\
@@ -15,22 +14,24 @@ $('#new-todo').keyup(function(e){ console.log(e.keyCode);
                 </div>\
            </li>');
         // Добавление в массив
-        tasks.push($this.val());
+        var $this = $(this);
+        var newTask = $this.val();
+        tasks.push(newTask);
         // Очистка Input
         $('#new-todo').val('');
     }
 });
 
+// Счетчик задач
+$(".todo-count").append('<strong>' + tasks.length + '</strong> items left');
+
 /* Удаление */
 
 $('#todo-list').on('click','button.destroy', function(){
-    var index = $(this).parent().find('label').text();
-    if( tasks.indexOf(index) > -1 ){
-        tasks.splice( tasks.indexOf( index ), 1);
-        $(this).parent().parent().remove();
-    }
-    if ( tasks.indexOf(index) < 1 ) {
+    var $li = $(this).closest('li');
+    tasks.splice($li.index(), 1);
+    $li.remove();
+    if ( tasks[0] == 0 ) {
         $('#footer').hide();
     }
 });
-
