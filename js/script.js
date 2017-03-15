a051243b9dc829b1ea9a2f70a15dd62e11c5849e
LoranDeMarcus <lorandemarcus@gmail.com>
2017-03-15 23:08:41 +0300

    добавил чекбокс. работает?

--- js/script.js
+++ js/script.js
@@ -2,38 +2,50 @@
 
 var tasks = [];
 
-$('#new-todo').keyup(function(event) {
+$( '#new-todo' ).keyup( function( event ) {
 
-    if (event.keyCode==13){
-        $('#main').show();
-        $('#footer').show();
-        $('#todo-list').append('<li>\
+    if ( event.keyCode==13 ) {
+        $( '#main' ).show();
+        $( '#footer' ).show();
+        $( '#todo-list' ).append( '<li>\
                 <div class="todo-task">\
-                  <label class="text"><input class="toggle" type="checkbox">'+$(this).val()+'</label>\
+                  <label class="text"><input class="toggle" type="checkbox">'+$( this ).val()+'</label>\
                     <button class="destroy"></button>\
                 </div>\
            </li>');
-        // Добавление id
-
         //Счетчик задач
-        $('strong').text(tasks.length + 1);
+        $( 'strong' ).text( tasks.length + 1 );
         // Добавление в массив
-        var $this = $(this);
+        var $this = $( this );
         var newTask = $this.val();
-        tasks.push(newTask);
+        tasks.push( newTask );
         // Очистка Input
-        $('#new-todo').val('');
+        $( '#new-todo' ).val('');
     }
 });
 
+// Checkbox
+
+$( 'li' ).on( 'click', 'input.checkbox', function () {
+    $( this ).toggleClass( 'checked' )
+})
+
+
+/* Проверка Checkbox
+
+if ($( 'input.toggle' ).is( ':checked' )) {
+
+}
+*/
+
 /* Удаление */
 
-$('#todo-list').on('click','button.destroy', function() {
-    var $li = $(this).closest('li');
-    tasks.splice($li.index(), 1);
+$( '#todo-list' ).on( 'click','button.destroy', function() {
+    var $li = $( this ).closest( 'li' );
+    tasks.splice( $li.index(), 1 );
     $li.remove();
-    $('strong').text(tasks.length);
+    $( 'strong' ).text( tasks.length );
     if ( tasks.length < 1 ) {
-        $('#footer').hide();
+        $( '#footer' ).hide();
     }
 });
