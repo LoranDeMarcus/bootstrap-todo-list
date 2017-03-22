b5bbfccc65e1145b79defad0a4fd658a8464de78
LoranDeMarcus <lorandemarcus@gmail.com>
2017-03-22 23:39:33 +0300

    добавил проверку на ввод пустой строки

--- js/script.js
+++ js/script.js
@@ -1,39 +1,38 @@
 var tasks = [];
 var i = 0;
-function updateTasks () {
-    $('#todo-list').remove("li");
-    $('#todo-list').append('<li data-id="'+ i +'">\
-            <div class="todo-task">\
-            <label    class="text"><input class="toggle" type="checkbox">' + $('#new-todo').val() + '</label>\
-            <button class="destroy"></button>\
-            </div>\
-            </li>');
-
-}
 
 /* Добавление задач */
 
 $('#new-todo').keyup(function (event) {
-    if (event.keyCode == 13) {
-        $('#main').show();
-        $('#footer').show();
-        $('.count').text(tasks.length + 1);
-        // Добавление в массив
-        var $this = $(this);
-        var newTask = $this.val();
-        tasks.push({id: i++, title: newTask, status: 'active'});
-        updateTasks();
-        // Очистка Input
-        $('#new-todo').val('');
-
+    if ((event.keyCode == 13)) {
+        if ($(this).val() !== '') {
+            $('#main').show();
+            $('#footer').show();
+            $('#todo-list').append('<li class="complete" data-id="'+ i +'">\
+            <div class="todo-task">\
+            <label class="text"><input class="toggle" type="checkbox">' + $(this).val() + '</label>\
+            <button class="destroy"></button>\
+            </div>\
+            </li>');
+            // Добавление в массив
+            var $this = $(this);
+            var newTask = $this.val();
+            tasks.push({id: i++, title: newTask, status: 'active'});
+            $('.count').text(tasks.length + 1);
+            // Очистка Input
+            $('#new-todo').val('');
+        }
+        else {
+            return false;
+        }
     }
 });
 
 /* Checkbox */
 
-$('#todo-list .toggle').on('change', function() {
-    $(this).parents('li').toggleClass('checked');
-});
+$('#todo-list .toggle').on('change', function () {
+    console.log('something');
+})
 
 $('#toggle-all').on('change', function () {
     if ($('#todo-list .toggle:checked').length == $('#todo-list .toggle').length) {
@@ -48,10 +47,9 @@ $('#toggle-all').on('change', function () {
 
 /* События кнопки Clear Completed */
 
-if ($('.completed').hasClass('checked')) {
+if ($('.complete').hasClass('checked')) {
     $('#footer').append('<button id="clear-completed">Clear completed</button>');
-}
-;
+};
 
 /* Удаление */
 
@@ -65,20 +63,10 @@ $('#todo-list').on('click', 'button.destroy', function () {
     }
 });
 
-/* Удаление выбраных задач */
 
 /* Временное хранилище
 
- $('#todo-list').append('<li class="completed">\
- <div class="todo-task">\
- <label class="text"><input class="toggle" type="checkbox">' + $(this).val() + '</label>\
- <button class="destroy"></button>\
- </div>\
- </li>');
-
+ $('#todo-list .complete .toggle .todo-task .text').on('click', function() {
  $(this).parents('li').toggleClass('checked');
-
- var $li = $(this).closest('li');
- $li.remove();
- tasks.splice($li.index(), 1);
+ console.log('id');
  */
